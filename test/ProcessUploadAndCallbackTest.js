7da4308e750199d328b4afc5dac89a76e794d555
Justin Tanner <justinwtanner@gmail.com>
2019-06-22 18:03:58 -0500

    Upgraded multiple dependencies

--- test/ProcessUploadAndCallbackTest.js
+++ test/ProcessUploadAndCallbackTest.js
@@ -36,38 +36,37 @@ test.serial("processes a valid jpg and calls back", t => {
 
   awsMocks.putObject(null, {});
 
-  return ProcessUploadAndCallback.fromPath(
-    validKey,
-    awsMocks.s3
-  ).catch(results => {
-    // This should not be catch, this should be a then!
-    t.is(
-      results.finalMessage,
-      "Successfully processed image. Created 2 styles."
-    );
+  return ProcessUploadAndCallback.fromPath(validKey, awsMocks.s3).catch(
+    results => {
+      // This should not be catch, this should be a then!
+      t.is(
+        results.finalMessage,
+        "Successfully processed image. Created 2 styles."
+      );
 
-    const expectedPostData = {
-      url: "http://lvh.me.org/null",
-      json: {
-        fileName: "t.jpg",
-        fileSize: 592,
-        contentType: "image/jpeg",
-        uniqueId: "2",
-        finishedPathPrefix: `finished/1/2`,
-        originalKey: validKey,
-        sizes: {
-          original: { width: 960, height: 720 },
-          thumb: { width: 100, height: 75 },
-          large: { width: 500, height: 375 }
-        },
-        anything: "goes"
-      }
-    };
+      const expectedPostData = {
+        url: "http://lvh.me.org/null",
+        json: {
+          fileName: "t.jpg",
+          fileSize: 592,
+          contentType: "image/jpeg",
+          uniqueId: "2",
+          finishedPathPrefix: `finished/1/2`,
+          originalKey: validKey,
+          sizes: {
+            original: { width: 960, height: 720 },
+            thumb: { width: 100, height: 75 },
+            large: { width: 500, height: 375 }
+          },
+          anything: "goes"
+        }
+      };
 
-    const actualPostData = requestSpy.args[0][0];
-    t.is(actualPostData.url, expectedPostData.url);
-    t.deepEqual(actualPostData.json, expectedPostData.json);
-  });
+      const actualPostData = requestSpy.args[0][0];
+      t.is(actualPostData.url, expectedPostData.url);
+      t.deepEqual(actualPostData.json, expectedPostData.json);
+    }
+  );
 });
 
 test.serial("calls back with an error when an image could not be found", t => {
@@ -75,10 +74,9 @@ test.serial("calls back with an error when an image could not be found", t => {
   let awsMocks = new AwsMocks();
   awsMocks.getObject(new Error("Faking a failed retrieval"), {});
 
-  return ProcessUploadAndCallback.fromPath(
-    validKey,
-    awsMocks.s3
-  ).catch(results => {
-    t.true(results.error.message.startsWith("Failed to fetch S3 Object."));
-  });
+  return ProcessUploadAndCallback.fromPath(validKey, awsMocks.s3).catch(
+    results => {
+      t.true(results.error.message.startsWith("Failed to fetch S3 Object."));
+    }
+  );
 });
